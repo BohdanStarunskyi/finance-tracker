12f4d3abfd4062599e4a39f4a9f5851bf2c5c5c8
Bohdan Starunskyi <bohdanstarunskyi@gmail.com>
2024-01-21 14:42:57 +0100

    fixed ssl

--- src/shared/config.ts
+++ src/shared/config.ts
@@ -3,18 +3,16 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 export const dbConfig: DataSourceOptions = {
-    type: 'postgres',
-    host: process.env.DB_HOST,
-    port: +process.env.DB_PORT,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    entities: ['dist/shared/entities/**/*.{ts,js}'],
-    migrations: ['dist/migrations/**/*.{ts,js}'],
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  };
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: +process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  entities: ['dist/shared/entities/**/*.{ts,js}'],
+  migrations: ['dist/migrations/**/*.{ts,js}'],
+  ssl: process.env.ENV === 'production' ? { rejectUnauthorized: false } : false,
+};
 
-  const dataSource: DataSource = new DataSource(dbConfig);
-  export default dataSource;
+const dataSource: DataSource = new DataSource(dbConfig);
+export default dataSource;
